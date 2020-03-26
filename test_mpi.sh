4edaa142936ed423544383d1f8f0fe6f91a3de1f
Timo Halbesma <timohalbesma@gmail.com>
2020-03-26 11:05:41 +0100

    echo which mpiexec, mpi4py version, and also test mpiexec.hydra

--- test_mpi.sh
+++ test_mpi.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-unset -e
+set -e
+
+which mpiexec
+python -c "import mpi4py; print(mpi4py)"
 
 echo -e "\nRunning mpiexec /w mpi4py test w/o hostfile"
-mpiexec -n 5 python -m mpi4py.bench helloworld
+mpiexec -n 1 python -m mpi4py.bench helloworld
 echo "mpiexec exit code: $?"  # unfortunately 1
 
 echo -e "\nRunning mpiexec /w mpi4py test with a hostfile"
 mpiexec --hostfile hostfile_mpich2 -n 5 python -m mpi4py.bench helloworld
+echo "mpiexec exit code: $?"  # should be 0
+
+echo "Check Hydra thingy"
+
+echo -e "\nRunning mpiexec.hydra /w mpi4py test w/o hostfile"
+mpiexec.hydra -n 5 python -m mpi4py.bench helloworld
+echo "mpiexec exit code: $?"  # unfortunately 1
+
+echo -e "\nRunning mpiexec.hydra /w mpi4py test with a hostfile"
+mpiexec.hydra --hostfile hostfile_mpich2 -n 5 python -m mpi4py.bench helloworld
 echo "mpiexec exit code: $?"  # should be 0
